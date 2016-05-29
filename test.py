d840e7363e80742e794d1d0bc16168a87454bd3d
zhaot777 <zhaot777@gmail.com>
2016-05-28 23:35:01 -0700

    Update test.py

--- test.py
+++ test.py
@@ -3,13 +3,10 @@
 import urllib
 import json
 import psycopg2
-from sqlalchemy.dialects.postgresql import JSON
 
 
-jw = open("output.txt", "wb")
-f = urllib.urlopen("https://www.dockstore.org:8443/api/v1/tools")
-jw.write(f.read())
-jw.close()
+
+
 
 conn = psycopg2.connect("dbname='mydb' user='postgres' password='123' host='localhost'")
 print "Opened database successfully"
@@ -17,21 +14,39 @@ print "Opened database successfully"
 cur = conn.cursor()
 
 cur.execute('''CREATE TABLE tools(
-	GLOBAL_ID	varchar primary key, 
-	registry-id varchar, 
-	  registry varchar,
-		organization varchar,
-		name varchar,
-		toolname varchar,
-		tooltype json,
-		description varchar,
-		author varchar,
-		meta-version varchar,
-		version varchar,
-		version global-id varchar
+	global-id	varcha	primary key, 
+	registry-id	varchar, 
+	registry	varchar,
+	organization	varchar,
+	name	varchar,
+	toolname	varchar,
+	tooltype	json,
+	description	varchar,
+	author	varchar,
+	meta-version	varchar,
+	version	varchar,
+	version-global-id	varchar
+       );''')
+       
+cur.execute('''CREATE TABLE tools_versions(
+	name	varchar,
+	global-id	varchar primary key, 
+	registry-id	varchar,
+	image	varchar,
+	descriptor	json,
+	dockerfile	json,
+	meta-version	varchar,
        );''')
 
+f = urllib.urlopen("https://www.dockstore.org:8443/api/v1/tools")
+json_f = json.loads(f.read())
 
+for i in json_f:
+	cur.execute("insert into tools(global-id, registry-id, \
+	registry, organization, name, toolname, tooltype, description, \
+	author, version-global-id) \
+	VALUES \
+	(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s);",(i[""])
 
 conn.commit()
 
